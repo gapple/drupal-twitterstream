2229a63600eb4c853e7b181f37c96d53aff48165
Geoff Appleby <geoff@gapple.ca>
2012-07-29 00:44:07 -0700

    Configurable CLI script for consuming Streaming API

---
+++ TwitterstreamPublicConsumer.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Consume tweet data from the Steaming API and store in the database.
+ */
+class TwitterstreamPublicConsumer extends Phirehose {
+  public $db = null;
+
+  /**
+   * Store the provided raw status to the database.
+   *
+   * @see Phirehose::enqueueStatus()
+   */
+  public function enqueueStatus($status) {
+    $this->db->query("INSERT INTO {twitterstream_raw} SET data = :data", array(':data' => $status));
+  }
+
+  /**
+   * Fetch the keywords to track and users to follow.
+   *
+   * @see Phirehose::checkFilterPredicates()
+   */
+  public function checkFilterPredicates() {
+
+    $track = array();
+    $follow = array();
+
+    // TODO can we prevent querying the database and rebuilding the full arrays each time?
+    $result = $this->db->query("SELECT module, params FROM {twitterstream_params}");
+    foreach ($result as $row) {
+      $params = unserialize($row->params);
+      if (!empty($params['track'])) {
+        $track = array_merge($track, $params['track']);
+      }
+      if (!empty($params['follow'])) {
+        $follow = array_merge($follow, $params['follow']);
+      }
+    }
+
+    $this->setTrack($track);
+    $this->setFollow($follow);
+  }
+}
---
+++ twitterstream_daemon.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * A Daemon script to set up a Streaming API consumer.
+ */
+
+// Make sure the deamon is not triggered from the web.
+if (php_sapi_name() != 'cli') {
+  exit();
+}
+
+if (preg_match('<(.*)/sites/[^/]+/modules/twitterstream>', dirname(__FILE__), $drupal_root)) {
+  $drupal_root = $drupal_root[1];
+}
+else if (preg_match('<(.*)/profiles/[^/]+/modules/twitterstream>', dirname(__FILE__), $drupal_root)) {
+  $drupal_root = $drupal_root[1];
+}
+else {
+  exit("Could not find Drupal root directory");
+}
+
+define('DRUPAL_ROOT', $drupal_root);
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+require_once('Phirehose.php');
+require_once('TwitterstreamPublicConsumer.php');
+
+// REMOTE_ADDR is not defined when called via CLI, but some Drupal functions
+// assume that it exists, so define it to avoid undefined index errors.
+$_SERVER['REMOTE_ADDR'] = null;
+
+drupal_bootstrap(DRUPAL_BOOTSTRAP_VARIABLES);
+
+$consumer = new TwitterstreamPublicConsumer(
+    variable_get('twitterstream_username'),
+    variable_get('twitterstream_password'),
+    Phirehose::METHOD_FILTER
+  );
+
+$consumer->db = Database::getConnection();
+
+$consumer->consume();
